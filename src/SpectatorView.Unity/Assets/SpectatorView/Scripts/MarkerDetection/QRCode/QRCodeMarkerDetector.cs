707a4198b7d587404d418662125d1f6b8b7fe30c
Chris Barth <chriba@microsoft.com>
2019-06-20 14:42:01 -0700

    center reported marker position for qr codes

--- src/SpectatorView.Unity/Assets/SpectatorView/Scripts/MarkerDetection/QRCode/QRCodeMarkerDetector.cs
+++ src/SpectatorView.Unity/Assets/SpectatorView/Scripts/MarkerDetection/QRCode/QRCodeMarkerDetector.cs
@@ -191,8 +191,15 @@ namespace Microsoft.MixedReality.Toolkit.Extensions.Experimental.SpectatorView.M
                         // Spectator view marker detectors should return a positive y axis up the marker,
                         // so, we rotate the marker orientation 180 degrees around its z axis.
                         var rotation = Quaternion.LookRotation(location.GetColumn(2), location.GetColumn(1)) * Quaternion.Euler(0, 0, 180);
-                        var marker = new Marker(markerId, translation, rotation);
-                        markerDictionary[markerId] = marker;
+
+                        if (_markerSizes.TryGetValue(markerId, out var size))
+                        {
+                            var transform = Matrix4x4.TRS(translation, rotation, Vector3.one);
+                            var offset = -1.0f * size / 2.0f;
+                            var markerCenter = transform.MultiplyPoint(new Vector3(offset, offset, 0));
+                            var marker = new Marker(markerId, markerCenter, rotation);
+                            markerDictionary[markerId] = marker;
+                        }
                     }
                 }
             }
