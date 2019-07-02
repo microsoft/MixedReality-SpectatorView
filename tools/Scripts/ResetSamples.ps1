26ead34d95ec379a3cdd4b52c6b4c849ccada7fe
Chris Barth <chriba@microsoft.com>
2019-07-02 13:13:22 -0700

    revert scripts and settings

--- tools/Scripts/ResetSamples.ps1
+++ tools/Scripts/ResetSamples.ps1
@@ -1,5 +1,4 @@
 git config core.symlinks true
 git submodule update --init
-dir '$PSScriptRoot\..\..\src' -Recurse -File | ?{$_.LinkType -eq "SymbolicLink" } | Remove-Item
 dir '$PSScriptRoot\..\..\samples' -Recurse -File | ?{$_.LinkType -eq "SymbolicLink" } | Remove-Item
 git checkout -f -- :/samples
--- tools/Scripts/ResetSamples.sh
+++ tools/Scripts/ResetSamples.sh
@@ -2,6 +2,5 @@
 
 git config core.symlinks true
 git submodule update --init
-find ../../src -type l | xargs rm
 find ../../samples -type l | xargs rm
 git checkout -f -- :/samples
