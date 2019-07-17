aa604fa25af6bd8320de5306238306ff5376ee8b
Chris Barth <chriba@microsoft.com>
2019-07-17 12:36:25 -0700

    update script and call out running as an administrator

--- tools/Scripts/SetupRepository.ps1
+++ tools/Scripts/SetupRepository.ps1
@@ -37,15 +37,23 @@ Set-Location (Split-Path $MyInvocation.MyCommand.Path)
 # for inter-directory linking to build the HolographicCamera.Unity project
 # as well as for the samples to include submodule repository content
 # via directory symlinks.
+Write-Output "Enabling symbolic link directories for the repository."
 git config core.symlinks true
 
-# Ensure that submodules are initialized and cloned
+# Ensure consistent line endings.
+Write-Output "Configuring the repository to use crlf line endings."
+git config core.autocrlf true
+
+# Ensure that submodules are initialized and cloned.
+Write-Output "Updating all submodules."
 git submodule update --init
 
 # If any links were created to the submodules but were broken,
 # restore those symlinks now that the submodules are cloned.
+Write-Output "Updating all symbolic link directories."
 dir "$PSScriptRoot\..\..\" -Recurse -File | ?{$_.LinkType -eq "SymbolicLink" } | Restore-SymbolicLink
 
 # If any links were created before the repository was configured to use
 # symlinks, those links need to be restored.
+Write-Output "Fixing any custom symbolic links."
 git status --porcelain | Restore-SymbolicLinkFromTypeChange
