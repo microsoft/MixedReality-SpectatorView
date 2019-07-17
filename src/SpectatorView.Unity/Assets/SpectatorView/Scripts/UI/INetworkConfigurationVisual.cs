8ea0d67cb19076dd9e7a09589f75a754578b5208
Chris Barth <chriba@microsoft.com>
2019-07-17 14:51:44 -0700

    new logic for setting up a network configuration visual on mobile

---
+++ src/SpectatorView.Unity/Assets/SpectatorView/Scripts/UI/INetworkConfigurationVisual.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace Microsoft.MixedReality.SpectatorView
+{
+    public delegate void NetworkConfigurationUpdatedHandler(object sender, string ipAddress);
+
+    public interface INetworkConfigurationVisual
+    {
+        /// <summary>
+        /// Called when the associated network configuration has been updated.
+        /// </summary>
+        event NetworkConfigurationUpdatedHandler NetworkConfigurationUpdated;
+    }
+}
---
+++ src/SpectatorView.Unity/Assets/SpectatorView/Scripts/UI/NetworkConfigurationSettings.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+
+namespace Microsoft.MixedReality.SpectatorView
+{
+    public class NetworkConfigurationSettings : Singleton<NetworkConfigurationSettings>
+    {
+        /// <summary>
+        /// Check to enable a mobile network configuration visual to obtain the user IP Address.
+        /// </summary>
+        [Tooltip("Check to enable a mobile network configuration visual to obtain the user IP Address.")]
+        [SerializeField]
+        private bool enableMobileNetworkConfigurationVisual = true;
+
+        /// <summary>
+        /// Prefab for creating a mobile network configuration visual.
+        /// </summary>
+        [Tooltip("Prefab for creating amobile network configuration visual, which replaces the defaultMobileNetworkConfigurationVisualPrefab on the SpectatorView component if set.")]
+        [SerializeField]
+        private GameObject overrideMobileNetworkConfigurationVisualPrefab = null;
+
+        public bool EnableMobileNetworkConfigurationVisual => enableMobileNetworkConfigurationVisual;
+
+        public GameObject OverrideMobileNetworkConfigurationVisualPrefab => overrideMobileNetworkConfigurationVisualPrefab;
+    }
+}
