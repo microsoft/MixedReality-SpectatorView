e4d45968726ec77d5f99d4ef5dfbf98108c26ca5
Matthew Johnson <mattjo@microsoft.com>
2019-07-18 08:54:10 -0700

    Remove unused namespace

--- src/SpectatorView.Native/SpectatorView.WinRTExtensions/SpectatorView.WinRTExtensions.cpp
+++ src/SpectatorView.Native/SpectatorView.WinRTExtensions/SpectatorView.WinRTExtensions.cpp
@@ -1,8 +1,6 @@
 ﻿#include "pch.h"
 #include "SpectatorView.WinRTExtensions.h"
 
-using namespace ABI::Windows::Perception::Spatial;
-
 // Because Marshal.GetObjectForIUnknown does not work when using the .NET Native compiler with IInspectable
 // objects, this method allows managed code to explicitly specify a type for the marshaller to query for.
 // Example usage from managed code for marshalling a SpatialCoordinateSystem using this method:
