6172ee17b07acaf1cb579686d8e3cf98466d1121
chrisfromwork <c.m.barth@outlook.com>
2019-07-29 15:46:49 -0700

    fix scripts to better share functionality

--- tools/Scripts/FixSymbolicLinks.ps1
+++ tools/Scripts/FixSymbolicLinks.ps1
@@ -1,20 +1,12 @@
 . "$PSScriptRoot\SymbolicLinkHelpers.ps1"
 
-Write-Output "Enabling symbolic links for the repository."
-git config core.symlinks true
+ConfigureRepo
 
 # Ensure that submodules are initialized and cloned.
 Write-Output "Updating all submodules."
-git submodule update --init
+git submodule update --init --recursive
 
-# If any links were created to the submodules but were broken,
-# restore those symlinks now that the submodules are cloned.
-Write-Output "Fixing all symbolic links."
-dir Get-Location -Recurse -File | ?{$_.LinkType -eq "SymbolicLink" } | Restore-SymbolicLink
-
-# If any links were created before the repository was configured to use
-# symlinks, those links need to be restored.
-git status --porcelain | Restore-SymbolicLinkFromTypeChange
+FixSymbolicLinks
 
 Write-Host "`n"
 Write-Host -NoNewLine 'Symbolic links fixed. Press any key to continue...';
--- tools/Scripts/SymbolicLinkHelpers.ps1
+++ tools/Scripts/SymbolicLinkHelpers.ps1
@@ -30,3 +30,31 @@ Function Restore-SymbolicLinkFromTypeChange {
         }
     }
 }
+
+Function ConfigureRepo {
+    Process {
+	# Enable symlink support for the repository. This is necessary
+	# for inter-directory linking to build the HolographicCamera.Unity project
+	# as well as for the samples to include submodule repository content
+	# via directory symlinks.
+	Write-Output "Enabling symbolic links for the repository."
+	git config core.symlinks true
+
+	# Ensure consistent line endings.
+	Write-Output "Configuring the repository to use crlf line endings."
+	git config core.autocrlf true
+    }
+}
+
+Function FixSymbolicLinks {
+    Process {
+	# If any links were created to the submodules but were broken,
+	# restore those symlinks now that the submodules are cloned.
+	Write-Output "Fixing all symbolic links."
+	dir Get-Location -Recurse -File | ?{$_.LinkType -eq "SymbolicLink" } | Restore-SymbolicLink
+
+	# If any links were created before the repository was configured to use
+	# symlinks, those links need to be restored.
+	git status --porcelain | Restore-SymbolicLinkFromTypeChange
+    }
+}
