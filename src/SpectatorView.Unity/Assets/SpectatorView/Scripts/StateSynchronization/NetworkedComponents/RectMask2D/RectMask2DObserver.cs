4a2bf7bd3d6161f93a78f87eef87b5e6d79d4d71
Matthew Johnson <mattjo@microsoft.com>
2019-07-29 14:45:43 -0700

    Add support for syncing RectMask2D components

---
+++ src/SpectatorView.Unity/Assets/SpectatorView/Scripts/StateSynchronization/NetworkedComponents/RectMask2D/RectMask2DObserver.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Microsoft.MixedReality.SpectatorView
+{
+    internal class RectMask2DObserver : ComponentObserver<RectMask2D>
+    {
+        public override void Read(SocketEndpoint sendingEndpoint, BinaryReader message)
+        {
+            RectMask2DBroadcaster.ChangeType changeType = (RectMask2DBroadcaster.ChangeType)message.ReadByte();
+
+            if (RectMask2DBroadcaster.HasFlag(changeType, RectMask2DBroadcaster.ChangeType.Properties))
+            {
+                if (attachedComponent == null)
+                {
+                    attachedComponent = gameObject.AddComponent<RectMask2D>();
+                }
+
+                attachedComponent.enabled = message.ReadBoolean();
+            }
+        }
+    }
+}
