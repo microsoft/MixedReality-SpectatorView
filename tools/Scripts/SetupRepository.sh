b0ad8081cc91afc7325017a5f1c9461b854fca2f
Chris Barth <chriba@Chriss-MacBook-Pro.local>
2019-08-13 15:56:49 -0700

    add ability to obtain zip file

--- tools/Scripts/SetupRepository.sh
+++ tools/Scripts/SetupRepository.sh
@@ -5,6 +5,20 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 git config core.symlinks true
 git config core.autocrlf true
+
+ZIPPATH="$SCRIPTPATH/../../external/ARKit-Unity-Plugin/ARKitUnityPlugin.zip"
+if [ ! -f $ZIPPATH ]
+then
+    echo "Obtaining ARKit-Unity-Plugin dependencies."
+    curl -o $ZIPPATH https://bitbucket.org/Unity-Technologies/unity-arkit-plugin/get/94e47eae5954.zip
+    echo "Extracting ARKit-Unity-Plugin dependencies."
+    TEMPPATH="$SCRIPTPATH/../../external/ARKit-Unity-Plugin/Temp"
+    mkdir $TEMPPATH
+    unzip $ZIPPATH -d $TEMPPATH
+    mv $TEMPPATH/Unity-Technologies-unity-arkit-plugin-94e47eae5954/* $TEMPPATH/../
+    rm -r $TEMPPATH
+fi
+
 git submodule update --init
 find $SCRIPTPATH/../../samples -type l | xargs rm
 git checkout -f -- :/samples
