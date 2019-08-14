84ab8ec2fdbe85f078bf3e61b837077051703d33
Chris Barth <chriba@Chriss-MacBook-Pro.local>
2019-08-14 10:08:44 -0700

    add ability to pull down arkit dependencies

--- tools/Scripts/SetupRepository.sh
+++ tools/Scripts/SetupRepository.sh
@@ -3,6 +3,7 @@
 # Directory to the build script
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
+echo "Configuring repo to support symbolic links and crlf line endings."
 git config core.symlinks true
 git config core.autocrlf true
 
@@ -19,6 +20,9 @@ then
     rm -r $TEMPPATH
 fi
 
+echo "Updating submodules."
 git submodule update --init
+
+echo "Fixing symbolic links."
 find $SCRIPTPATH/../../samples -type l | xargs rm
 git checkout -f -- :/samples
