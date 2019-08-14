7ccee1592eb04dfb52db8c233b868f1919ae79f0
chrisfromwork <c.m.barth@outlook.com>
2019-08-14 15:38:46 -0700

    add better helpers for obtaining external dependencies

--- tools/Scripts/ExternalDependencyHelpers.ps1
+++ tools/Scripts/ExternalDependencyHelpers.ps1
@@ -1,8 +1,9 @@
 function DownloadQRCodePlugin
 {
-  if (!(Test-Path "$PSScriptRoot\..\..\external\MixedReality-QRCodePlugin\release"))
+  $zipFile = "$PSScriptRoot\..\..\external\MixedReality-QRCodePlugin\qrcodeplugin.zip"
+  if (!(Test-Path $zipFile))
   {
-    $zipFile = "$PSScriptRoot\..\..\external\qrcodeplugin.zip"
+    Write-Host "Populating QR Code Dependencies"
     $url = "https://github.com/dorreneb/mixed-reality/releases/download/1.1/release.zip"
     $wc = New-Object System.Net.WebClient
     $wc.DownloadFile($url, $zipFile)
@@ -16,16 +17,19 @@ function DownloadQRCodePlugin
 
 function DownloadARKitPlugin
 {
-  if (!(Test-Path "$PSScriptRoot\..\..\external\ARKit-Unity-Plugin\Unity-Technologies-unity-arkit-plugin-94e47eae5954"))
+  $zipFile = "$PSScriptRoot\..\..\external\ARKit-Unity-Plugin\unity-arkit-plugin.zip"
+  if (!(Test-Path $zipFile))
   {
-    $zipFile = "$PSScriptRoot\..\..\external\unity-arkit-plugin.zip"
+    Write-Host "Populating ARKit Dependencies"
     $url = "https://bitbucket.org/Unity-Technologies/unity-arkit-plugin/get/94e47eae5954.zip"
     $wc = New-Object System.Net.WebClient
     $wc.DownloadFile($url, $zipFile)
-    Expand-Archive -Path $zipFile -DestinationPath "$PSScriptRoot\..\..\external\ARKit-Unity-Plugin" -Force
+    Expand-Archive -Path $zipFile -DestinationPath "$PSScriptRoot\..\..\external\ARKit-Unity-Plugin\Temp" -Force
+    Move-Item -Path "$PSScriptRoot\..\..\external\ARKit-Unity-Plugin\Temp\Unity-Technologies-unity-arkit-plugin-94e47eae5954\*" -Destination "$PSScriptRoot\..\..\external\ARKit-Unity-Plugin"
+    Remove-Item -Path "$PSScriptRoot\..\..\external\ARKit-Unity-Plugin\Temp" -Recurse
   }
   else
   {
-    Write-Host "external/ARKit-Unity-Plugin already populated in repo."
+    Write-Host "external/ARKit-Unity-Plugin already populated in repo"
   }
 }
