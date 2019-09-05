c133b447151dbb188976ac00b4de8a7acaf1f3fd
chrisfromwork <c.m.barth@outlook.com>
2019-09-05 15:41:38 -0700

    setup first tests for compositor

---
+++ src/SpectatorView.Unity/Assets/SpectatorView.Tests/CompositionManagerTests.cs
@@ -0,0 +1,181 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.Video;
+
+namespace Microsoft.MixedReality.SpectatorView.Tests
+{
+    [UnityPlatform(RuntimePlatform.WindowsEditor)]
+    public class CompositionManagerTests : CompositorTestsBase
+    {
+        const double recordTimeInSeconds = 5.0;
+        const double videoLengthVariability = 2.0;
+        const int compositeVideoWidth = 1920;
+        const int compositeVideoHeight = 1080;
+        const int quadVideoWidth = compositeVideoWidth * 2;
+        const int quadVideoHeight = compositeVideoHeight * 2;
+        const int numVideos = 3;
+
+        [UnityTest]
+        public IEnumerator TextureInitializationBlackmagicDesignTest()
+        {
+            yield return CompositionManager.CaptureDevice = FrameProviderDeviceType.BlackMagic;
+            Assert.AreEqual(CompositionManager.CaptureDevice, FrameProviderDeviceType.BlackMagic, "Set CaptureDevice as BlackMagic");
+            yield return AssertTexturesInitialize("Blackmagic Design");
+        }
+
+        [UnityTest]
+        public IEnumerator TextureInitializationElgatoTest()
+        {
+            yield return CompositionManager.CaptureDevice = FrameProviderDeviceType.Elgato;
+            Assert.AreEqual(CompositionManager.CaptureDevice, FrameProviderDeviceType.Elgato, "Set CaptureDevice as Elgato");
+            yield return AssertTexturesInitialize("Elgato");
+        }
+
+        [UnityTest]
+        public IEnumerator RecordCompositeVideoTest()
+        {
+            yield return SetupCompositeRecording();
+
+            float startTime = Time.time;
+
+            string videoFilePath = CompositionManager.StartRecording();
+            Debug.Log($"Recording file: {videoFilePath}");
+            filesToDelete.Add(videoFilePath);
+            while (Time.time - startTime < recordTimeInSeconds)
+            {
+                yield return null;
+            }
+            CompositionManager.StopRecording();
+            yield return null;
+
+            yield return AssertVideoFileParams(videoFilePath, recordTimeInSeconds, compositeVideoWidth, compositeVideoHeight);
+        }
+
+        [UnityTest]
+        public IEnumerator RecordMultipleCompositeVideosTest()
+        {
+            yield return SetupCompositeRecording();
+
+            for (int n = 0; n < numVideos; n++)
+            {
+                float startTime = Time.time;
+
+                Debug.Log($"Recording Composite Video: {n}");
+                string videoFilePath = CompositionManager.StartRecording();
+                Debug.Log($"Recording file: {videoFilePath}");
+                filesToDelete.Add(videoFilePath);
+                while (Time.time - startTime < recordTimeInSeconds)
+                {
+                    yield return null;
+                }
+                CompositionManager.StopRecording();
+                yield return null;
+
+                yield return AssertVideoFileParams(videoFilePath, recordTimeInSeconds, compositeVideoWidth, compositeVideoHeight);
+            }
+        }
+
+        [UnityTest]
+        public IEnumerator RecordQuadVideoTest()
+        {
+            yield return SetupQuadRecording();
+
+            float startTime = Time.time;
+
+            string videoFilePath = CompositionManager.StartRecording();
+            Debug.Log($"Recording file: {videoFilePath}");
+            filesToDelete.Add(videoFilePath);
+            while (Time.time - startTime < recordTimeInSeconds)
+            {
+                yield return null;
+            }
+            CompositionManager.StopRecording();
+            yield return null;
+
+            yield return AssertVideoFileParams(videoFilePath, recordTimeInSeconds, quadVideoWidth, quadVideoHeight);
+        }
+
+        [UnityTest]
+        public IEnumerator RecordMultipleQuadVideosTest()
+        {
+            yield return SetupQuadRecording();
+
+            for (int n = 0; n < numVideos; n++)
+            {
+                float startTime = Time.time;
+
+                Debug.Log($"Recording Quad Video: {n}");
+                string videoFilePath = CompositionManager.StartRecording();
+                Debug.Log($"Recording file: {videoFilePath}");
+                filesToDelete.Add(videoFilePath);
+                while (Time.time - startTime < recordTimeInSeconds)
+                {
+                    yield return null;
+                }
+                CompositionManager.StopRecording();
+                yield return null;
+
+                yield return AssertVideoFileParams(videoFilePath, recordTimeInSeconds, quadVideoWidth, quadVideoHeight);
+            }
+
+        }
+
+        private IEnumerator AssertVideoFileParams(string filePath, double videoLengthInSeconds, int width, int height)
+        {
+            VideoPlayer player = CompositionManager.gameObject.AddComponent<VideoPlayer>();
+            Assert.NotNull(player, "Player is not null.");
+            Debug.Log($"Loading video file: {filePath}");
+            player.url = filePath;
+            WaitForSeconds waitTime = new WaitForSeconds(1);
+            player.Prepare();
+            while (!player.isPrepared)
+            {
+                Debug.Log("Player is not yet prepared...");
+                yield return waitTime;
+            }
+
+            player.Play();
+            while (!player.isPlaying)
+            {
+                Debug.Log("waitTime is not yet playing...");
+                yield return null;
+            }
+
+            Debug.Log($"Asserting Video File Params, File:{player.url}, Length:{player.length}, Width:{player.width}, Height:{player.height}");
+            Assert.IsTrue(
+                (player.length > videoLengthInSeconds - videoLengthVariability) &&
+                (player.length < videoLengthInSeconds + videoLengthVariability),
+                $"Video length was within expected range: ({videoLengthInSeconds - videoLengthVariability},{videoLengthInSeconds + videoLengthVariability}), actual:{player.length}");
+            Assert.IsTrue(
+                (player.width == width) &&
+                (player.height == height),
+                $"Video dimensions were expected value: {width}x{height}, actual:{player.width}x{player.height}");
+
+            player.Stop();
+            while (player.isPlaying)
+            {
+                Debug.Log("Player is not yet stopped...");
+                yield return waitTime;
+            }
+            Component.Destroy(player);
+        }
+
+        private IEnumerator SetupCompositeRecording()
+        {
+            yield return CompositionManager.CaptureDevice = FrameProviderDeviceType.BlackMagic;
+            Assert.AreEqual(CompositionManager.CaptureDevice, FrameProviderDeviceType.BlackMagic, "Set CaptureDevice as Blackmagic");
+
+            yield return CompositionManager.VideoRecordingLayout = VideoRecordingFrameLayout.Composite;
+        }
+
+        private IEnumerator SetupQuadRecording()
+        {
+            yield return CompositionManager.CaptureDevice = FrameProviderDeviceType.BlackMagic;
+            Assert.AreEqual(CompositionManager.CaptureDevice, FrameProviderDeviceType.BlackMagic, "Set CaptureDevice as Blackmagic");
+
+            yield return CompositionManager.VideoRecordingLayout = VideoRecordingFrameLayout.Quad;
+        }
+    }
+}
---
+++ src/SpectatorView.Unity/Assets/SpectatorView.Tests/CompositorTestsBase.cs
@@ -0,0 +1,97 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Microsoft.MixedReality.SpectatorView.Tests
+{
+    public class CompositorTestsBase
+    {
+        public CompositionManager CompositionManager
+        {
+            get
+            {
+                if (compositionManager == null)
+                {
+                    compositionManager = GameObject.FindObjectOfType<CompositionManager>();
+                }
+
+                return compositionManager;
+            }
+        }
+        private CompositionManager compositionManager;
+        private string originalSceneName;
+        protected List<string> filesToDelete = new List<string>();
+        protected GameObject helperGameObject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            originalSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene("SpectatorViewCompositor");
+            helperGameObject = new GameObject("HelperGameObject");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            CompositionManager.TextureManager.SetOverrideColorTexture(null);
+            CompositionManager.TextureManager.SetOverrideOutputTexture(null);
+            SceneManager.LoadScene(originalSceneName);
+            foreach (var file in filesToDelete)
+            {
+                if (File.Exists(file))
+                {
+                    Debug.Log($"Deleting file: {file}");
+                    File.Delete(file);
+                }
+            }
+        }
+
+        protected IEnumerator AssertTexturesInitialize(string captureDeviceName)
+        {
+            yield return CompositionManager.VideoRecordingLayout = VideoRecordingFrameLayout.Composite;
+            AssertAllCompositeTexturesCreated(captureDeviceName);
+            yield return CompositionManager.VideoRecordingLayout = VideoRecordingFrameLayout.Quad;
+            AssertAllQuadTexturesCreated(captureDeviceName);
+        }
+
+        protected void AssertAllCompositeTexturesCreated(string captureDeviceName)
+        {
+            Assert.IsNotNull(CompositionManager.TextureManager.colorRGBTexture, $"{captureDeviceName}: Composite ColorRGBTexture is not null.");
+            AssertTextureSize(CompositionManager.TextureManager.colorRGBTexture, CompositionManager.GetVideoFrameWidth(), CompositionManager.GetVideoFrameHeight());
+            Assert.IsNotNull(CompositionManager.TextureManager.renderTexture, $"{captureDeviceName}: Composite RenderTextrue is not null.");
+            AssertTextureSize(CompositionManager.TextureManager.renderTexture, CompositionManager.GetVideoFrameWidth(), CompositionManager.GetVideoFrameHeight());
+            Assert.IsNotNull(CompositionManager.TextureManager.compositeTexture, $"{captureDeviceName}: Composite CompositeTexture is not null.");
+            AssertTextureSize(CompositionManager.TextureManager.compositeTexture, CompositionManager.GetVideoFrameWidth(), CompositionManager.GetVideoFrameHeight());
+            Assert.IsNotNull(CompositionManager.TextureManager.previewTexture, $"{captureDeviceName}: Composite PreviewTexture is not null.");
+            AssertTextureSize(CompositionManager.TextureManager.previewTexture, CompositionManager.GetVideoFrameWidth(), CompositionManager.GetVideoFrameHeight());
+            Assert.IsNotNull(CompositionManager.TextureManager.alphaTexture, $"{captureDeviceName}: Composite AlphaTexture is not null.");
+            AssertTextureSize(CompositionManager.TextureManager.alphaTexture, CompositionManager.GetVideoFrameWidth(), CompositionManager.GetVideoFrameHeight());
+        }
+
+        protected void AssertAllQuadTexturesCreated(string captureDeviceName)
+        {
+            Assert.IsNotNull(CompositionManager.TextureManager.colorRGBTexture, $"{captureDeviceName}: Quad ColorRGBTexture is not null.");
+            AssertTextureSize(CompositionManager.TextureManager.colorRGBTexture, CompositionManager.GetVideoFrameWidth(), CompositionManager.GetVideoFrameHeight());
+            Assert.IsNotNull(CompositionManager.TextureManager.renderTexture, $"{captureDeviceName}: Quad RenderTexture is not null.");
+            AssertTextureSize(CompositionManager.TextureManager.renderTexture, CompositionManager.GetVideoFrameWidth(), CompositionManager.GetVideoFrameHeight());
+            Assert.IsNotNull(CompositionManager.TextureManager.compositeTexture, $"{captureDeviceName}: Quad CompositeTexture is not null.");
+            AssertTextureSize(CompositionManager.TextureManager.compositeTexture, CompositionManager.GetVideoFrameWidth(), CompositionManager.GetVideoFrameHeight());
+            Assert.IsNotNull(CompositionManager.TextureManager.previewTexture, $"{captureDeviceName}: Quad PreviewTexture is not null.");
+            AssertTextureSize(CompositionManager.TextureManager.previewTexture, CompositionManager.GetVideoFrameWidth(), CompositionManager.GetVideoFrameHeight());
+            Assert.IsNotNull(CompositionManager.TextureManager.alphaTexture, $"{captureDeviceName}: Quad AlphaTexture is not null.");
+            AssertTextureSize(CompositionManager.TextureManager.alphaTexture, CompositionManager.GetVideoFrameWidth(), CompositionManager.GetVideoFrameHeight());
+            Assert.IsNotNull(CompositionManager.TextureManager.quadViewOutputTexture, $"{captureDeviceName}: Quad QuadViewOutputTexture is not null.");
+            AssertTextureSize(CompositionManager.TextureManager.quadViewOutputTexture, CompositionManager.VideoRecordingFrameWidth, CompositionManager.VideoRecordingFrameHeight);
+        }
+
+        protected void AssertTextureSize(Texture texture, int width, int height)
+        {
+            Assert.AreEqual(texture.width, width, "Texture Width");
+            Assert.AreEqual(texture.height, height, "Texture Height");
+        }
+    }
+}
