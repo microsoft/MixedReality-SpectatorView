3dfd57c02a97d70a590519c2673d5926ddbc532e
chrisfromwork <c.m.barth@outlook.com>
2019-10-17 15:05:10 -0700

    enable multiscene experiences

---
+++ src/SpectatorView.Unity/Assets/SpectatorView/Scripts/Test/SceneSwitcher.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Microsoft.MixedReality.SpectatorView
+{
+    internal class SceneSwitcher : MonoBehaviour
+    {
+        [Tooltip("List of scenes to toggle between.")]
+        [SerializeField]
+        private string[] scenes = new string[]
+        {
+            "SpectatorView.TestScene1",
+            "SpectatorView.TestScene2"
+        };
+
+        [Tooltip("Amount of time to spend in each scene")]
+        [SerializeField]
+        private float sceneDurationInSeconds = 5.0f;
+
+        private float timeUntilNextSceneTransition = 0.0f;
+        private int currentSceneIndex = 0;
+
+        void Awake()
+        {
+            DontDestroyOnLoad(this);
+        }
+
+        void Start()
+        {
+            timeUntilNextSceneTransition = sceneDurationInSeconds;
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            bool sceneFound = false;
+            for (int i = 0; i < scenes.Length; i++)
+            {
+                if (scenes[i] == currentSceneName)
+                {
+                    currentSceneIndex = i;
+                    sceneFound = true;
+                    return;
+                }
+            }
+
+            if (sceneFound)
+            {
+                Debug.LogWarning("Current scene was found in scene list. This may result in multiple instances of Spectator View getting created");
+            }
+        }
+
+        void Update()
+        {
+            timeUntilNextSceneTransition -= Time.deltaTime;
+            if (timeUntilNextSceneTransition < 0)
+            {
+                string nextSceneName = scenes[currentSceneIndex];
+                try
+                {
+                    SceneManager.LoadScene(nextSceneName);
+                }
+                catch { }
+
+                currentSceneIndex++;
+                currentSceneIndex = currentSceneIndex >= scenes.Length ? 0 : currentSceneIndex;
+                timeUntilNextSceneTransition = sceneDurationInSeconds;
+            }
+        }
+    }
+}
+
