503317d5654a29b6054f566cd6f95b414fbae7e9
Robert El-Soudani <mrbobbybobberson@gmail.com>
2019-10-21 13:27:24 -0700

    Avoiding warnings about "missing scripts" on platform device by instantiating them from prefabs.
      * Originally, all devices were in the scene but disabled, and during bootstrap, only the correct device was enabled, but that lead to missing script warnings for platforms with absent plugins.  By taking them as prefab references and only instantiating the correct platform device, we don't get any warnings.

---
+++ src/GenericSpectator/Assets/Scripts/PlatformDeviceBootstrapper.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class PlatformDeviceBootstrapper : MonoBehaviour
+{
+    /// <summary>
+    /// The device prefab for Windows platforms.
+    /// </summary>
+    [Tooltip("The device prefab for Windows platforms.")]
+    [SerializeField]
+    protected GameObject windowsDevicePrefab;
+
+    /// <summary>
+    /// The device prefab for the Android platform.
+    /// </summary>
+    [Tooltip("The device prefab for the Android platform.")]
+    [SerializeField]
+    protected GameObject androidDevicePrefab;
+
+    /// <summary>
+    /// The device prefab for the iOS platform.
+    /// </summary>
+    [Tooltip("The device prefab for the iOS platform.")]
+    [SerializeField]
+    protected GameObject iOSDevicePrefab;
+
+    protected void Awake()
+    {
+        GameObject devicePrfab;
+
+#if UNITY_WSA || UNITY_STANDALONE_WIN
+        devicePrfab = windowsDevicePrefab;
+#elif UNITY_ANDROID
+        devicePrfab = androidDevicePrefab;
+#elif UNITY_IOS
+        devicePrfab = iOSDevicePrefab;
+#else
+        Debug.LogError($"There is no device prefab for the current build platform: {Application.platform}.  Please select a different build platform or add a device prefab for this one.", this);
+        return;
+#endif
+
+        if (devicePrfab == null)
+        {
+            Debug.LogError($"The device prefab isn't set for the current build platform. Please select a different build platform or set the device prefab.", this);
+            return;
+        }
+
+        Instantiate(devicePrfab, transform);
+    }
+}
