7e4346c1e0928120094b8fe92ec157a980f73629
Robert El-Soudani <mrbobbybobberson@gmail.com>
2019-10-22 12:48:11 -0700

    Adding copyright, namespace, and typo fix to PlatformDeviceBootstrapper.

--- src/GenericSpectator/Assets/Scripts/PlatformDeviceBootstrapper.cs
+++ src/GenericSpectator/Assets/Scripts/PlatformDeviceBootstrapper.cs
@@ -1,49 +1,55 @@
-﻿using UnityEngine;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
 
-public class PlatformDeviceBootstrapper : MonoBehaviour
+using UnityEngine;
+
+namespace Microsoft.MixedReality.SpectatorView
 {
-    /// <summary>
-    /// The device prefab for Windows platforms.
-    /// </summary>
-    [Tooltip("The device prefab for Windows platforms.")]
-    [SerializeField]
-    protected GameObject windowsDevicePrefab;
-
-    /// <summary>
-    /// The device prefab for the Android platform.
-    /// </summary>
-    [Tooltip("The device prefab for the Android platform.")]
-    [SerializeField]
-    protected GameObject androidDevicePrefab;
-
-    /// <summary>
-    /// The device prefab for the iOS platform.
-    /// </summary>
-    [Tooltip("The device prefab for the iOS platform.")]
-    [SerializeField]
-    protected GameObject iOSDevicePrefab;
-
-    protected void Awake()
+    public class PlatformDeviceBootstrapper : MonoBehaviour
     {
-        GameObject devicePrfab;
+        /// <summary>
+        /// The device prefab for Windows platforms.
+        /// </summary>
+        [Tooltip("The device prefab for Windows platforms.")]
+        [SerializeField]
+        protected GameObject windowsDevicePrefab;
+
+        /// <summary>
+        /// The device prefab for the Android platform.
+        /// </summary>
+        [Tooltip("The device prefab for the Android platform.")]
+        [SerializeField]
+        protected GameObject androidDevicePrefab;
+
+        /// <summary>
+        /// The device prefab for the iOS platform.
+        /// </summary>
+        [Tooltip("The device prefab for the iOS platform.")]
+        [SerializeField]
+        protected GameObject iOSDevicePrefab;
+
+        protected void Awake()
+        {
+            GameObject devicePrefab;
 
 #if UNITY_WSA || UNITY_STANDALONE_WIN
-        devicePrfab = windowsDevicePrefab;
+            devicePrefab = windowsDevicePrefab;
 #elif UNITY_ANDROID
-        devicePrfab = androidDevicePrefab;
+            devicePrefab = androidDevicePrefab;
 #elif UNITY_IOS
-        devicePrfab = iOSDevicePrefab;
+            devicePrefab = iOSDevicePrefab;
 #else
-        Debug.LogError($"There is no device prefab for the current build platform: {Application.platform}.  Please select a different build platform or add a device prefab for this one.", this);
-        return;
+            Debug.LogError($"There is no device prefab for the current build platform: {Application.platform}.  Please select a different build platform or add a device prefab for this one.", this);
+            return;
 #endif
 
-        if (devicePrfab == null)
-        {
-            Debug.LogError($"The device prefab isn't set for the current build platform. Please select a different build platform or set the device prefab.", this);
-            return;
-        }
+            if (devicePrefab == null)
+            {
+                Debug.LogError($"The device prefab isn't set for the current build platform. Please select a different build platform or set the device prefab.", this);
+                return;
+            }
 
-        Instantiate(devicePrfab, transform);
+            Instantiate(devicePrefab, transform);
+        }
     }
 }
