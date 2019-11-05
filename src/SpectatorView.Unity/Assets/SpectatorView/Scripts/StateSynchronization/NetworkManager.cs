0c4950e59a5f4560c9fda65f69daba16881b63bc
Chris Barth <chriba@microsoft.com>
2019-11-05 14:24:45 -0800

    fix namespaces

--- src/SpectatorView.Unity/Assets/SpectatorView/Scripts/StateSynchronization/NetworkManager.cs
+++ src/SpectatorView.Unity/Assets/SpectatorView/Scripts/StateSynchronization/NetworkManager.cs
@@ -4,8 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using UnityEditor.MemoryProfiler;
 using UnityEngine;
 
 namespace Microsoft.MixedReality.SpectatorView
