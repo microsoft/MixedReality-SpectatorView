e088d2757af565f9378d222ab78c50959a8e4827
Jacob <hsoo6373@uni.sydney.edu.au>
2019-11-20 02:46:30 +1100

    Updated header of all scripts

--- samples/ProjectGrandmaster.Unity/Assets/Script/Board/BoardInformation.cs
+++ samples/ProjectGrandmaster.Unity/Assets/Script/Board/BoardInformation.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
+// Contributed by USYD Team - Hrithvik (Jacob) Sood, John Tran, Tom Derrick, Aydin Ucan, Aayush Jindal
 
 using System.Collections;
 using System.Collections.Generic;
