b03e2f7a3cfc077c090db43851297aca856108c1
Jacob <hsoo6373@uni.sydney.edu.au>
2019-11-20 02:46:30 +1100

    Updated header of all scripts

--- samples/ProjectGrandmaster.Unity/Assets/Script/HighlightObjects/HighlightChessPiece.cs
+++ samples/ProjectGrandmaster.Unity/Assets/Script/HighlightObjects/HighlightChessPiece.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// Contributed by USYD Team - Hrithvik (Jacob) Sood, John Tran, Tom Derrick, Aydin Ucan, Aayush Jindal
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
