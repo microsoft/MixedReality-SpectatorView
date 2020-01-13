2c9ed9a9bddde9b6c2369b9d1208fc07ed787433
Chris Barth <chriba@Chriss-MBP.guest.corp.microsoft.com>
2020-01-13 12:07:58 -0800

    fix tools/qr code detection for mac

--- src/SpectatorView.Unity/Assets/SpectatorView/Scripts/MarkerDetection/QRCode/QRCodeMarkerDetector.cs
+++ src/SpectatorView.Unity/Assets/SpectatorView/Scripts/MarkerDetection/QRCode/QRCodeMarkerDetector.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-#if WINDOWS_UWP && UNITY_WSA
+#if WINDOWS_UWP && UNITY_WSA && !UNITY_EDITOR_OSX
 #define ENABLE_QRCODES
 #endif
 
