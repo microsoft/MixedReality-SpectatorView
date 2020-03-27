2a5a362306af2d02189b0c60598b305cda71d37a
Matthew Johnson <mattjo@microsoft.com>
2020-03-27 10:59:33 -0700

    Add instructions and scripts for detecting an ArUco marker pattern that aligns with a QR code for HoloLens 2

---
+++ src/SpectatorView.Unity/Assets/SpectatorView/Scripts/SpatialAlignment/PhysicalMarker/ArUcoThreeMarkerSpatialLocalizerSettings.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.MixedReality.SpectatorView;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.SpectatorView
+{
+    public class ArUcoThreeMarkerLocalizationSettings : ISpatialLocalizationSettings, IEditableSpatialLocalizationSettings
+    {
+        public int TopMarkerID { get; set; } = 0;
+        public int MiddleMarkerID { get; set; } = 1;
+        public int BottomMarkerID { get; set; } = 2;
+        public float MarkerSize { get; set; } = 0.1f;
+
+        public void Serialize(BinaryWriter writer)
+        {
+            writer.Write(TopMarkerID);
+            writer.Write(MiddleMarkerID);
+            writer.Write(BottomMarkerID);
+            writer.Write(MarkerSize);
+        }
+
+        public static bool TryDeserialize(BinaryReader reader, out ArUcoThreeMarkerLocalizationSettings settings)
+        {
+            try
+            {
+                settings = new ArUcoThreeMarkerLocalizationSettings
+                {
+                    TopMarkerID = reader.ReadInt32(),
+                    MiddleMarkerID = reader.ReadInt32(),
+                    BottomMarkerID = reader.ReadInt32(),
+                    MarkerSize = reader.ReadSingle(),
+                };
+                return true;
+            }
+            catch
+            {
+                settings = null;
+                return false;
+            }
+        }
+
+#if UNITY_EDITOR
+        public SpatialLocalizationSettingsEditor CreateEditor()
+        {
+            return new Editor(this);
+        }
+
+        private class Editor : SpatialLocalizationSettingsEditor
+        {
+            private ArUcoThreeMarkerLocalizationSettings settings;
+
+            public Editor(ArUcoThreeMarkerLocalizationSettings settings)
+            {
+                this.settings = settings;
+            }
+
+            public override void OnGUI(Rect rect)
+            {
+                settings.TopMarkerID = EditorGUILayout.IntField("Top Marker ID", settings.TopMarkerID);
+                settings.MiddleMarkerID = EditorGUILayout.IntField("Middle Marker ID", settings.MiddleMarkerID);
+                settings.BottomMarkerID = EditorGUILayout.IntField("Bottom Marker ID", settings.BottomMarkerID);
+                settings.MarkerSize = EditorGUILayout.FloatField("Marker Size (m)", settings.MarkerSize);
+            }
+        }
+#endif
+    }
+}
