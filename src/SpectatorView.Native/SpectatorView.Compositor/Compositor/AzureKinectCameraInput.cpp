644e555f58e22106227a41d08d3a5179f766a2d4
Matthew Johnson <mattjo@microsoft.com>
2020-04-02 13:03:44 -0700

    Make underscores consistent for fields

--- src/SpectatorView.Native/SpectatorView.Compositor/Compositor/AzureKinectCameraInput.cpp
+++ src/SpectatorView.Native/SpectatorView.Compositor/Compositor/AzureKinectCameraInput.cpp
@@ -14,28 +14,28 @@
 AzureKinectCameraInput::AzureKinectCameraInput(k4a_depth_mode_t depthMode, bool captureDepth, bool captureBodyMask)
     : _captureDepth(captureDepth)
     , _captureBodyMask(captureBodyMask)
-    , depthCameraMode(depthMode)
-    , calibration()
-    , k4aDevice(nullptr)
-    , transformation(nullptr)
-    , transformedDepthImage(nullptr)
-    , transformedBodyMaskImage(nullptr)
-    , bodyMaskImage(nullptr)
+    , _depthCameraMode(depthMode)
+    , _calibration()
+    , _k4aDevice(nullptr)
+    , _transformation(nullptr)
+    , _transformedDepthImage(nullptr)
+    , _transformedBodyMaskImage(nullptr)
+    , _bodyMaskImage(nullptr)
     , _stopRequested(false)
     , _currentFrameIndex(0)
-    , detectMarkers(false)
-    , markerSize(0.0f)
-    , markerDictionaryName(cv::aruco::DICT_6X6_250)
-    , markerDetector(new ArUcoMarkerDetector())
+    , _detectMarkers(false)
+    , _markerSize(0.0f)
+    , _markerDictionaryName(cv::aruco::DICT_6X6_250)
+    , _markerDetector(new ArUcoMarkerDetector())
     , _colorImageStride(0)
     , _depthImageStride(0)
     , _bodyMaskImageStride(0)
 #if defined(INCLUDE_AZUREKINECT_BODYTRACKING)
     , _currentBodyMaskFrameIndex(0)
-    , k4abtTracker(nullptr)
+    , _k4abtTracker(nullptr)
 #endif
 {
-    if (K4A_RESULT_SUCCEEDED != k4a_device_open(K4A_DEVICE_DEFAULT, &k4aDevice))
+    if (K4A_RESULT_SUCCEEDED != k4a_device_open(K4A_DEVICE_DEFAULT, &_k4aDevice))
     {
         OutputDebugString(L"Failed to open AzureKinect device");
         goto FailedExit;
@@ -46,18 +46,18 @@ AzureKinectCameraInput::AzureKinectCameraInput(k4a_depth_mode_t depthMode, bool 
         _cameraFrames[i] = new AzureKinectCameraFrame(captureDepth, captureBodyMask);
     }
 
-    config.color_format = K4A_IMAGE_FORMAT_COLOR_BGRA32;
-    config.color_resolution = K4A_COLOR_RESOLUTION_1080P;
-    config.depth_mode = depthCameraMode;
-    config.camera_fps = K4A_FRAMES_PER_SECOND_30;
+    _config.color_format = K4A_IMAGE_FORMAT_COLOR_BGRA32;
+    _config.color_resolution = K4A_COLOR_RESOLUTION_1080P;
+    _config.depth_mode = _depthCameraMode;
+    _config.camera_fps = K4A_FRAMES_PER_SECOND_30;
 
-    if (K4A_RESULT_SUCCEEDED != k4a_device_start_cameras(k4aDevice, &config))
+    if (K4A_RESULT_SUCCEEDED != k4a_device_start_cameras(_k4aDevice, &_config))
     {
         OutputDebugString(L"Failed to start AzureKinect camera");
         goto FailedExit;
     }
 
-    if (K4A_RESULT_SUCCEEDED != k4a_device_get_calibration(k4aDevice, config.depth_mode, config.color_resolution, &calibration))
+    if (K4A_RESULT_SUCCEEDED != k4a_device_get_calibration(_k4aDevice, _config.depth_mode, _config.color_resolution, &_calibration))
     {
         OutputDebugString(L"Failed to get camera calibration");
         goto FailedExit;
@@ -65,24 +65,24 @@ AzureKinectCameraInput::AzureKinectCameraInput(k4a_depth_mode_t depthMode, bool 
 
     if (captureDepth)
     {
-        transformation = k4a_transformation_create(&calibration);
-        k4a_image_create(K4A_IMAGE_FORMAT_DEPTH16, calibration.color_camera_calibration.resolution_width, calibration.color_camera_calibration.resolution_height, 2 * calibration.color_camera_calibration.resolution_width, &transformedDepthImage);
-        _depthImageStride = k4a_image_get_stride_bytes(transformedDepthImage);
+        _transformation = k4a_transformation_create(&_calibration);
+        k4a_image_create(K4A_IMAGE_FORMAT_DEPTH16, _calibration.color_camera_calibration.resolution_width, _calibration.color_camera_calibration.resolution_height, 2 * _calibration.color_camera_calibration.resolution_width, &_transformedDepthImage);
+        _depthImageStride = k4a_image_get_stride_bytes(_transformedDepthImage);
 
 #if defined(INCLUDE_AZUREKINECT_BODYTRACKING)
         if (captureBodyMask)
         {
             k4abt_tracker_configuration_t tracker_config = K4ABT_TRACKER_CONFIG_DEFAULT;
-            if (K4A_RESULT_SUCCEEDED != k4abt_tracker_create(&calibration, tracker_config, &k4abtTracker))
+            if (K4A_RESULT_SUCCEEDED != k4abt_tracker_create(&_calibration, tracker_config, &_k4abtTracker))
             {
                 OutputDebugString(L"Body tracker initialization failed!\n");
                 goto FailedExit;
             }
 
             // Create new depth texture for body depth only
-            k4a_image_create(K4A_IMAGE_FORMAT_DEPTH16, calibration.depth_camera_calibration.resolution_width, calibration.depth_camera_calibration.resolution_height, 2 * calibration.depth_camera_calibration.resolution_width, &bodyMaskImage);
-            k4a_image_create(K4A_IMAGE_FORMAT_DEPTH16, calibration.color_camera_calibration.resolution_width, calibration.color_camera_calibration.resolution_height, 2 * calibration.color_camera_calibration.resolution_width, &transformedBodyMaskImage);
-            _bodyMaskImageStride = k4a_image_get_stride_bytes(transformedBodyMaskImage);
+            k4a_image_create(K4A_IMAGE_FORMAT_DEPTH16, _calibration.depth_camera_calibration.resolution_width, _calibration.depth_camera_calibration.resolution_height, 2 * _calibration.depth_camera_calibration.resolution_width, &_bodyMaskImage);
+            k4a_image_create(K4A_IMAGE_FORMAT_DEPTH16, _calibration.color_camera_calibration.resolution_width, _calibration.color_camera_calibration.resolution_height, 2 * _calibration.color_camera_calibration.resolution_width, &_transformedBodyMaskImage);
+            _bodyMaskImageStride = k4a_image_get_stride_bytes(_transformedBodyMaskImage);
         }
 #endif
 
@@ -91,15 +91,18 @@ AzureKinectCameraInput::AzureKinectCameraInput(k4a_depth_mode_t depthMode, bool 
     _thread = std::make_shared<std::thread>(std::bind(&AzureKinectCameraInput::RunCaptureLoop, this));
 
 #if defined(INCLUDE_AZUREKINECT_BODYTRACKING)
-    _bodyIndexThread = std::make_shared<std::thread>(std::bind(&AzureKinectCameraInput::RunBodyIndexLoop, this));
+    if (captureBodyMask)
+    {
+        _bodyIndexThread = std::make_shared<std::thread>(std::bind(&AzureKinectCameraInput::RunBodyIndexLoop, this));
+    }
 #endif
 
     return;
 
 FailedExit:
-    if (k4aDevice != NULL)
+    if (_k4aDevice != NULL)
     {
-        k4a_device_close(k4aDevice);
+        k4a_device_close(_k4aDevice);
     }
 }
 
@@ -108,40 +111,43 @@ AzureKinectCameraInput::~AzureKinectCameraInput()
     _stopRequested = true;
     _thread->join();
 
-#if defined(INCLUDE_AZUREKINECT_BODYTRACKING) 
-    _bodyIndexThread->join();
+#if defined(INCLUDE_AZUREKINECT_BODYTRACKING)
+    if (_bodyIndexThread != nullptr)
+    {
+        _bodyIndexThread->join();
+    }
 #endif
 
-    if (bodyMaskImage != nullptr)
+    if (_bodyMaskImage != nullptr)
     {
-        k4a_image_release(bodyMaskImage);
-        bodyMaskImage = nullptr;
+        k4a_image_release(_bodyMaskImage);
+        _bodyMaskImage = nullptr;
     }
 
-    if (transformedBodyMaskImage != nullptr)
+    if (_transformedBodyMaskImage != nullptr)
     {
-        k4a_image_release(transformedBodyMaskImage);
-        transformedBodyMaskImage = nullptr;
+        k4a_image_release(_transformedBodyMaskImage);
+        _transformedBodyMaskImage = nullptr;
     }
 
-    if (transformedDepthImage != nullptr)
+    if (_transformedDepthImage != nullptr)
     {
-        k4a_image_release(transformedDepthImage);
-        transformedDepthImage = nullptr;
+        k4a_image_release(_transformedDepthImage);
+        _transformedDepthImage = nullptr;
     }
 
-    if (k4aDevice != nullptr)
+    if (_k4aDevice != nullptr)
     {
-        k4a_device_stop_cameras(k4aDevice);
-        k4a_device_close(k4aDevice);
-        k4aDevice = nullptr;
+        k4a_device_stop_cameras(_k4aDevice);
+        k4a_device_close(_k4aDevice);
+        _k4aDevice = nullptr;
     }
 
 #if defined(INCLUDE_AZUREKINECT_BODYTRACKING)
-    if (k4abtTracker != nullptr)
+    if (_k4abtTracker != nullptr)
     {
-        k4abt_tracker_shutdown(k4abtTracker);
-        k4abt_tracker_destroy(k4abtTracker);
+        k4abt_tracker_shutdown(_k4abtTracker);
+        k4abt_tracker_destroy(_k4abtTracker);
     }
 #endif
 
@@ -167,7 +173,7 @@ void AzureKinectCameraInput::RunCaptureLoop()
 
         k4a_capture_t capture = nullptr;
 
-        switch (k4a_device_get_capture(k4aDevice, &capture, K4A_WAIT_INFINITE))
+        switch (k4a_device_get_capture(_k4aDevice, &capture, K4A_WAIT_INFINITE))
         {
         case K4A_WAIT_RESULT_TIMEOUT:
             OutputDebugString(L"Error: Timed out waiting for AzureKinect capture");
@@ -189,8 +195,8 @@ void AzureKinectCameraInput::RunCaptureLoop()
                 auto depthImage = k4a_capture_get_depth_image(capture);
                 if (depthImage != nullptr)
                 {
-                    k4a_transformation_depth_image_to_color_camera(transformation, depthImage, transformedDepthImage);
-                    _cameraFrames[frameIndex]->StageImage(AzureKinectImageType::Depth, transformedDepthImage);
+                    k4a_transformation_depth_image_to_color_camera(_transformation, depthImage, _transformedDepthImage);
+                    _cameraFrames[frameIndex]->StageImage(AzureKinectImageType::Depth, _transformedDepthImage);
 
 #if defined(INCLUDE_AZUREKINECT_BODYTRACKING)
                     if (_captureBodyMask)
@@ -199,7 +205,7 @@ void AzureKinectCameraInput::RunCaptureLoop()
                         // This will put the frame in a state where it's waiting for the body mask
                         // before entering the Staged state.
                         _cameraFrames[frameIndex]->BeginWritingBodyMask();
-                        k4a_wait_result_t queue_capture_result = k4abt_tracker_enqueue_capture(k4abtTracker, capture, K4A_WAIT_INFINITE);
+                        k4a_wait_result_t queue_capture_result = k4abt_tracker_enqueue_capture(_k4abtTracker, capture, K4A_WAIT_INFINITE);
 
                         if (queue_capture_result == K4A_WAIT_RESULT_FAILED)
                         {
@@ -245,9 +251,9 @@ bool AzureKinectCameraInput::UpdateSRVs(int frameIndex, ID3D11Device* device, ID
 
 void AzureKinectCameraInput::UpdateArUcoMarkers(k4a_image_t image)
 {
-    std::lock_guard<std::mutex> lockGuard(markerDetectorLock);
+    std::lock_guard<std::mutex> lockGuard(_markerDetectorLock);
 
-    if (detectMarkers)
+    if (_detectMarkers)
     {
         auto height = k4a_image_get_height_pixels(image);
         auto width = k4a_image_get_width_pixels(image);
@@ -256,56 +262,56 @@ void AzureKinectCameraInput::UpdateArUcoMarkers(k4a_image_t image)
         const int radialDistortionCount = 6;
         const int tangentialDistortionCount = 2;
 
-        float focalLength[2] = { calibration.color_camera_calibration.intrinsics.parameters.param.fx, calibration.color_camera_calibration.intrinsics.parameters.param.fy };
-        float principalPoint[2] = { calibration.color_camera_calibration.intrinsics.parameters.param.cx, calibration.color_camera_calibration.intrinsics.parameters.param.cy };
-        float radialDistortion[radialDistortionCount] = { calibration.color_camera_calibration.intrinsics.parameters.param.k1, calibration.color_camera_calibration.intrinsics.parameters.param.k2, calibration.color_camera_calibration.intrinsics.parameters.param.k3, calibration.color_camera_calibration.intrinsics.parameters.param.k4, calibration.color_camera_calibration.intrinsics.parameters.param.k5, calibration.color_camera_calibration.intrinsics.parameters.param.k6 };
-        float tangentialDistortion[tangentialDistortionCount] = { calibration.color_camera_calibration.intrinsics.parameters.param.p1, calibration.color_camera_calibration.intrinsics.parameters.param.p2 };
-        markerDetector->DetectMarkers(buffer, width, height, focalLength, principalPoint, radialDistortion, radialDistortionCount, tangentialDistortion, tangentialDistortionCount, markerSize, markerDictionaryName);
+        float focalLength[2] = { _calibration.color_camera_calibration.intrinsics.parameters.param.fx, _calibration.color_camera_calibration.intrinsics.parameters.param.fy };
+        float principalPoint[2] = { _calibration.color_camera_calibration.intrinsics.parameters.param.cx, _calibration.color_camera_calibration.intrinsics.parameters.param.cy };
+        float radialDistortion[radialDistortionCount] = { _calibration.color_camera_calibration.intrinsics.parameters.param.k1, _calibration.color_camera_calibration.intrinsics.parameters.param.k2, _calibration.color_camera_calibration.intrinsics.parameters.param.k3, _calibration.color_camera_calibration.intrinsics.parameters.param.k4, _calibration.color_camera_calibration.intrinsics.parameters.param.k5, _calibration.color_camera_calibration.intrinsics.parameters.param.k6 };
+        float tangentialDistortion[tangentialDistortionCount] = { _calibration.color_camera_calibration.intrinsics.parameters.param.p1, _calibration.color_camera_calibration.intrinsics.parameters.param.p2 };
+        _markerDetector->DetectMarkers(buffer, width, height, focalLength, principalPoint, radialDistortion, radialDistortionCount, tangentialDistortion, tangentialDistortionCount, _markerSize, _markerDictionaryName);
     }
 }
 
 void AzureKinectCameraInput::StartArUcoMarkerDetector(cv::aruco::PREDEFINED_DICTIONARY_NAME markerDictionaryName, float markerSize)
 {
-    std::lock_guard<std::mutex> lockGuard(markerDetectorLock);
+    std::lock_guard<std::mutex> lockGuard(_markerDetectorLock);
 
-    this->markerDetector->Reset();
-    this->markerDictionaryName = markerDictionaryName;
-    this->markerSize = markerSize;
-    this->detectMarkers = true;
+    this->_markerDetector->Reset();
+    this->_markerDictionaryName = markerDictionaryName;
+    this->_markerSize = markerSize;
+    this->_detectMarkers = true;
 }
 
 void AzureKinectCameraInput::StopArUcoMarkerDetector()
 {
-    std::lock_guard<std::mutex> lockGuard(markerDetectorLock);
+    std::lock_guard<std::mutex> lockGuard(_markerDetectorLock);
 
-    this->detectMarkers = false;
+    this->_detectMarkers = false;
 }
 
 void AzureKinectCameraInput::GetLatestArUcoMarkers(int size, Marker* markers)
 {
-    std::lock_guard<std::mutex> lockGuard(markerDetectorLock);
+    std::lock_guard<std::mutex> lockGuard(_markerDetectorLock);
 
-    if (this->detectMarkers)
+    if (this->_detectMarkers)
     {
-        int localSize = markerDetector->GetDetectedMarkersCount();
+        int localSize = _markerDetector->GetDetectedMarkersCount();
         std::vector<int> markerIds;
         markerIds.resize(localSize);
-        markerDetector->GetDetectedMarkerIds(markerIds.data(), localSize);
+        _markerDetector->GetDetectedMarkerIds(markerIds.data(), localSize);
 
         for (int i = 0; i < localSize && i < size; i++)
         {
             markers[i].id = markerIds[i];
-            markerDetector->GetDetectedMarkerPose(markerIds[i], &markers[i].position, &markers[i].rotation);
+            _markerDetector->GetDetectedMarkerPose(markerIds[i], &markers[i].position, &markers[i].rotation);
         }
     }
 }
 
 void AzureKinectCameraInput::GetCameraCalibrationInformation(CameraIntrinsics* calibration)
 {
-    calibration->focalLength = { this->calibration.color_camera_calibration.intrinsics.parameters.param.fx, this->calibration.color_camera_calibration.intrinsics.parameters.param.fy };
-    calibration->principalPoint = { this->calibration.color_camera_calibration.intrinsics.parameters.param.cx, this->calibration.color_camera_calibration.intrinsics.parameters.param.cy };
-    calibration->imageWidth = this->calibration.color_camera_calibration.resolution_width;
-    calibration->imageHeight = this->calibration.color_camera_calibration.resolution_height;
+    calibration->focalLength = { this->_calibration.color_camera_calibration.intrinsics.parameters.param.fx, this->_calibration.color_camera_calibration.intrinsics.parameters.param.fy };
+    calibration->principalPoint = { this->_calibration.color_camera_calibration.intrinsics.parameters.param.cx, this->_calibration.color_camera_calibration.intrinsics.parameters.param.cy };
+    calibration->imageWidth = this->_calibration.color_camera_calibration.resolution_width;
+    calibration->imageHeight = this->_calibration.color_camera_calibration.resolution_height;
 }
 
 #if defined(INCLUDE_AZUREKINECT_BODYTRACKING)
@@ -314,12 +320,12 @@ void AzureKinectCameraInput::RunBodyIndexLoop()
     while (!_stopRequested)
     {
         k4abt_frame_t bodyFrame;
-        k4a_wait_result_t pop_frame_result = k4abt_tracker_pop_result(k4abtTracker, &bodyFrame, 0);
+        k4a_wait_result_t pop_frame_result = k4abt_tracker_pop_result(_k4abtTracker, &bodyFrame, BODY_INDEX_WAIT_TIME_MILLISECONDS);
         if (pop_frame_result == K4A_WAIT_RESULT_SUCCEEDED)
         {
-            uint16_t* bodyMaskBuffer = reinterpret_cast<uint16_t*>(k4a_image_get_buffer(bodyMaskImage));
-            auto height = k4a_image_get_height_pixels(bodyMaskImage);
-            auto width = k4a_image_get_width_pixels(bodyMaskImage);
+            uint16_t* bodyMaskBuffer = reinterpret_cast<uint16_t*>(k4a_image_get_buffer(_bodyMaskImage));
+            auto height = k4a_image_get_height_pixels(_bodyMaskImage);
+            auto width = k4a_image_get_width_pixels(_bodyMaskImage);
 
             k4a_image_t bodyIndexMap = k4abt_frame_get_body_index_map(bodyFrame);
             uint8_t* bodyIndexBuffer = k4a_image_get_buffer(bodyIndexMap);
@@ -331,12 +337,12 @@ void AzureKinectCameraInput::RunBodyIndexLoop()
             }
             ReleaseBodyIndexMap(bodyFrame, bodyIndexMap);
 
-            k4a_result_t result = k4a_transformation_depth_image_to_color_camera(transformation, bodyMaskImage, transformedBodyMaskImage);
+            k4a_result_t result = k4a_transformation_depth_image_to_color_camera(_transformation, _bodyMaskImage, _transformedBodyMaskImage);
 
-            height = k4a_image_get_height_pixels(transformedBodyMaskImage);
-            width = k4a_image_get_width_pixels(transformedBodyMaskImage);
+            height = k4a_image_get_height_pixels(_transformedBodyMaskImage);
+            width = k4a_image_get_width_pixels(_transformedBodyMaskImage);
 
-            bodyMaskBuffer = reinterpret_cast<uint16_t*>(k4a_image_get_buffer(transformedBodyMaskImage));
+            bodyMaskBuffer = reinterpret_cast<uint16_t*>(k4a_image_get_buffer(_transformedBodyMaskImage));
 
             // Set transformed body mask buffer to 1 where bodies are not recognized  
             SetTransformedBodyMaskBuffer(bodyMaskBuffer, height * width);
@@ -344,7 +350,7 @@ void AzureKinectCameraInput::RunBodyIndexLoop()
             // Stage the body mask image, and then end the WritingBodyMask state.
             // This will transition the frame to the Staged state.
             int frameIndex = _currentBodyMaskFrameIndex % MAX_NUM_CACHED_BUFFERS;
-            _cameraFrames[frameIndex]->StageImage(AzureKinectImageType::BodyMask, transformedBodyMaskImage);
+            _cameraFrames[frameIndex]->StageImage(AzureKinectImageType::BodyMask, _transformedBodyMaskImage);
             _cameraFrames[frameIndex]->EndWritingBodyMask();
 
             _currentBodyMaskFrameIndex++;
--- src/SpectatorView.Native/SpectatorView.Compositor/Compositor/AzureKinectCameraInput.h
+++ src/SpectatorView.Native/SpectatorView.Compositor/Compositor/AzureKinectCameraInput.h
@@ -15,7 +15,7 @@
 #include <k4abt.h>
 #endif
 #define MAX_NUM_CACHED_BUFFERS 20
-
+#define BODY_INDEX_WAIT_TIME_MILLISECONDS 500
 // Reads and buffers input from the Azure Kinect camera into a circular buffer.
 // The input threads stage AzureKinectCameraFrames, which contain buffered copies
 // of the color, depth, and body index images for that frame.
@@ -40,7 +40,7 @@ public:
     void GetCameraCalibrationInformation(CameraIntrinsics* calibration);
     void StartArUcoMarkerDetector(cv::aruco::PREDEFINED_DICTIONARY_NAME markerDictionaryName, float markerSize);
     void StopArUcoMarkerDetector();
-    int GetLatestArUcoMarkerCount() { return markerDetector->GetDetectedMarkersCount(); }
+    int GetLatestArUcoMarkerCount() { return _markerDetector->GetDetectedMarkersCount(); }
     void GetLatestArUcoMarkers(int size, Marker* markers);
 
     bool UpdateSRVs(int frameIndex, ID3D11Device* device, ID3D11ShaderResourceView* colorSRV, ID3D11ShaderResourceView* depthSRV, ID3D11ShaderResourceView* bodySRV);
@@ -51,20 +51,31 @@ private:
 
     std::atomic_bool _captureDepth;
     std::atomic_bool _captureBodyMask;
-    k4a_device_t k4aDevice;
-    k4a_device_configuration_t config = K4A_DEVICE_CONFIG_INIT_DISABLE_ALL;
-    k4a_calibration_t calibration;
-    k4a_transformation_t transformation;
-    k4a_image_t transformedDepthImage;
-    k4a_image_t transformedBodyMaskImage;
-    k4a_image_t bodyMaskImage;
-    k4a_depth_mode_t depthCameraMode = K4A_DEPTH_MODE_OFF;
+    k4a_device_t _k4aDevice;
+    k4a_device_configuration_t _config = K4A_DEVICE_CONFIG_INIT_DISABLE_ALL;
+    k4a_calibration_t _calibration;
+    k4a_transformation_t _transformation;
+    k4a_image_t _transformedDepthImage;
+    k4a_image_t _transformedBodyMaskImage;
+    k4a_image_t _bodyMaskImage;
+    k4a_depth_mode_t _depthCameraMode = K4A_DEPTH_MODE_OFF;
 
     AzureKinectCameraFrame* _cameraFrames[MAX_NUM_CACHED_BUFFERS];
 
     std::atomic_int _colorImageStride;
     int _depthImageStride;
     int _bodyMaskImageStride;
+
+    std::shared_ptr<std::thread> _thread;
+    std::atomic_bool _stopRequested;
+    std::atomic_int32_t _currentFrameIndex;
+
+    std::atomic_bool _detectMarkers;
+    std::atomic<float> _markerSize;
+    cv::aruco::PREDEFINED_DICTIONARY_NAME _markerDictionaryName;
+
+    std::mutex _markerDetectorLock;
+    std::shared_ptr<ArUcoMarkerDetector> _markerDetector;
 
 #if defined(INCLUDE_AZUREKINECT_BODYTRACKING)
     void RunBodyIndexLoop();
@@ -73,21 +84,10 @@ private:
     void SetBodyMaskBuffer(uint16_t* bodyMaskBuffer, uint8_t* bodyIndexBuffer, int bufferSize);
     void SetTransformedBodyMaskBuffer(uint16_t* transformedBodyMaskBuffer, int bufferSize);
 
-    k4abt_tracker_t k4abtTracker;
-    k4abt_tracker_configuration_t tracker_config = K4ABT_TRACKER_CONFIG_DEFAULT;
+    k4abt_tracker_t _k4abtTracker;
+    k4abt_tracker_configuration_t _tracker_config = K4ABT_TRACKER_CONFIG_DEFAULT;
     std::shared_ptr<std::thread> _bodyIndexThread;
     std::atomic_int32_t _currentBodyMaskFrameIndex;
 #endif
-
-    std::shared_ptr<std::thread> _thread;
-    std::atomic_bool _stopRequested;
-    std::atomic_int32_t _currentFrameIndex;
-
-    std::atomic_bool detectMarkers;
-    float markerSize;
-    cv::aruco::PREDEFINED_DICTIONARY_NAME markerDictionaryName;
-
-    std::mutex markerDetectorLock;
-    std::shared_ptr<ArUcoMarkerDetector> markerDetector;
 };
 #endif
